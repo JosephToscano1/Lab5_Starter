3a4845301bf25691326d935dfc23761c7fe77c38
roycehuang <130111798+roycehuang@users.noreply.github.com>
2024-05-07 20:55:07 -0700

    sum.test.js changed

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,5 +1,11 @@
 // sum.test.js
+import { sum } from '../code-to-unit-test/sum';
 
+//check if 1+2 = 3
 test('adds 1 + 2 to equal 3', () => {
-  // TODO
+  expect(1 + 2).toBe(3);
+});
+//check if sum(1,2) returns 3
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1,2)).toBe(3);
 });
